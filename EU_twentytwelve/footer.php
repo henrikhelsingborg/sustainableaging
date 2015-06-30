47abc1d7d99c322124447b4840ea0b869e0f96ef
henrikhelsingborg <henrik.wilhelmsson@helsingborg.se>
2015-06-30 14:49:44 +0200

    first upload - based on twentytwelve
    Child theme based on previously created Gröna idéer child theme - which
    is a child theme to old Twenty Twelve.

---
+++ EU_twentytwelve/footer.php
@@ -0,0 +1,50 @@
+﻿<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'twentytwelve_credits' ); ?>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
+		</div><!-- .site-info -->
+		<div class="footertext">
+			<div class="footerheading">
+
+				<br/>
+			</div>
+			<div class="leftfooter">
+				<b>Sustainable Ageing in Future Europe</b><br/>
+
+				Tampere, Finland<br/>
+				Foligno, Italy<br/>
+				WVO, Netherlands<br/>
+
+				<br/>
+				<br/>
+
+			</div>
+			<div class="rightfooter">
+
+
+			</div>
+			<div class="middlefooter">
+				
+			</div>
+
+		</div>
+
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
